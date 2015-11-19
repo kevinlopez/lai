629d0ef591a825246df32ed7cabd90f1b6164d30
kevinlopez <kevinlopeztorres@hotmail.com>
2015-11-19 12:38:59 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,6 +1,3 @@
 <?php
-String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
-String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-    $conexion = new mysqli(host,'admin5kmctQs','nHGaBf1j5yTS','lai',port);
-		
-			?>
+    $conexion = new mysqli('127.11.240.130','admin5kmctQs','nHGaBf1j5yTS','lai');
+?>
