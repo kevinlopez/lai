7e18c10c088f8549951eca0423c601a2d55ba2df
kevinlopez <kevinlopeztorres@hotmail.com>
2015-11-19 11:53:45 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli('OPENSHIFT_MYSQL_DB_HOST','admin5kmctQs','nHGaBf1j5yTS','lai',3306);
+    $conexion = new mysqli('127.6.28.2','admin5kmctQs','nHGaBf1j5yTS','lai',3306);
 		
 			?>
