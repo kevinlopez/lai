d548b58a71a0c52371aab4ccbe1ff7e35cd0bbd4
kevinlopez <kevinlopeztorres@hotmail.com>
2015-11-19 11:55:08 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
-    $conexion = new mysqli('127.6.28.2','admin5kmctQs','nHGaBf1j5yTS','lai',3306);
+    $conexion = new mysqli('127.6.28.2:3306','admin5kmctQs','nHGaBf1j5yTS','lai');
 		
 			?>
